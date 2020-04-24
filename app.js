2dd3842339a6673fcb1a2ceaa4ee4534a1ffea71
Julissa <a314783@uach.mx>
2020-04-23 21:05:04 -0600

    Cambios en mis archivos

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const log4js = require('log4js');
 
 const logger = log4js.getLogger();
-logger.level="error";
+logger.level="debug";
+
+function sumar(n1,n2){
+	return n1+n2;
+	}
 
 logger.debug("Mi primer app con npm");
 logger.info("Se ejecuto correctamente mi porceso.");
 logger.warn("No se encontro el archivo esperado.");
 logger.error("La aplicacion no se pudo conectar a la bd.");
 logger.fatal("La aplicacio no se pudo conectar a la bd");
+
+module.exports = sumar;
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+exports.default = function(){
+	return gulp.src(['app.js'])
+	.pipe(eslint())
+	.pipe(eslint.format())
+	}
